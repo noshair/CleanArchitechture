56a45670952f2eba940add55a025e1863a8506cd
Noshair Amjad <noshairamjad7@gmail.com>
2023-01-27 16:52:01 +0100

    Retrofit Improvement

--- app/src/main/java/com/cleanarchitechture/di/ApiServiceDataSource.kt
+++ app/src/main/java/com/cleanarchitechture/di/ApiServiceDataSource.kt
@@ -10,7 +10,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,22 +19,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class ApiServiceDataSource {
-    private val authInterceptor = Interceptor { chain ->
-        val newUrl = chain.request().url()
-            .newBuilder()
-            .build()
 
-        val newRequest = chain.request()
-            .newBuilder()
-            .url(newUrl)
-            .build()
-        chain.proceed(newRequest)
-    }
 
     //OkhttpClient for building http request url
 
     private val client = OkHttpClient().newBuilder()
-        .addInterceptor(authInterceptor)
         .connectTimeout(120, TimeUnit.SECONDS)
         .readTimeout(120, TimeUnit.SECONDS)
         .writeTimeout(120, TimeUnit.SECONDS)
